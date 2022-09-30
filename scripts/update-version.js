96786258ec4d18b7d792f143062f76e57813f510
diegokraenau <diegokraenau@gmail.com>
2022-09-30 16:33:23 -0500

    sfsdf

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -23,30 +23,30 @@ if (preversion[1]) {
   currentVersion += `-${preversion[1]}`;
 }
 
-// const patch = `${p}.${n}.${chalk.bold.dim.yellowBright(+m + 1)}`;
-// const minor = `${p}.${chalk.bold.dim.yellowBright(+n + 1)}.0`;
-// const major = `${chalk.bold.dim.yellowBright(+p + 1)}.0.0`;
+const patch = `${p}.${n}.${chalk.bold.dim.yellowBright(+m + 1)}`;
+const minor = `${p}.${chalk.bold.dim.yellowBright(+n + 1)}.0`;
+const major = `${chalk.bold.dim.yellowBright(+p + 1)}.0.0`;
 
-// const preUpdate = `${p}.${n}.${m}-${chalk.bold.dim.yellowBright(+preversion[1] + 1 || 0)}`;
+const preUpdate = `${p}.${n}.${m}-${chalk.bold.dim.yellowBright(+preversion[1] + 1 || 0)}`;
 
 const question = [{
   type: 'select',
   name: 'version',
-  message: `Seleccione la versión a subir. (Actual)`,
+  message: `Seleccione la versión a subir. (Actual: ${currentVersion})`,
   choices: [{
-      title: `Patch`,
+      title: `Patch: ${patch}`,
       value: 'patch'
     },
     {
-      title: `Minor`,
+      title: `Minor: ${minor}`,
       value: 'minor'
     },
     {
-      title: `Major`,
+      title: `Major: ${major}`,
       value: 'major'
     },
     {
-      title: `Preupdate`,
+      title: `Preupdate: ${preUpdate}`,
       value: 'preupdate'
     }
   ]
@@ -61,9 +61,9 @@ const question = [{
     process.exit(1);
   }
 
-//   analizeResponse(response);
-//   updatePackageJsonVersion();
-//   writePackageJson();
+  analizeResponse(response);
+  updatePackageJsonVersion();
+  writePackageJson();
 
 
 })();
@@ -80,103 +80,103 @@ function validateWorkspace() {
 //   }
 }
 
-// function writePackageJson() {
+function writePackageJson() {
 
-//   fs.writeFile('./package.json', JSON.stringify(packageJson, null, 2), function (err) {
-//     if (err) {
-//       console.log('Error actualizando el archivo', err);
-//       process.exit(1);
-//     }
-//     console.log(chalk.bold('Actualizada versión del proyecto'));
-//     console.log(chalk.bold('Creando commit de actualización'));
-//     gitUpdate();
-//     console.log(chalk.bold('Tag de Git creado con éxito'));
-//     // gitPush();
-//     process.exit(0);
-//   });
-// }
+  fs.writeFile('./package.json', JSON.stringify(packageJson, null, 2), function (err) {
+    if (err) {
+      console.log('Error actualizando el archivo', err);
+      process.exit(1);
+    }
+    console.log(chalk.bold('Actualizada versión del proyecto'));
+    console.log(chalk.bold('Creando commit de actualización'));
+    gitUpdate();
+    console.log(chalk.bold('Tag de Git creado con éxito'));
+    // gitPush();
+    process.exit(0);
+  });
+}
 
-// function updatePackageJsonVersion() {
-//   packageJson.version = version.join('.');
-//   if (preversion[1]) {
-//     packageJson.version += `-${preversion[1]}`;
-//   }
-// }
+function updatePackageJsonVersion() {
+  packageJson.version = version.join('.');
+  if (preversion[1]) {
+    packageJson.version += `-${preversion[1]}`;
+  }
+}
 
-// function gitPush() {
+function gitPush() {
 
-//   const gitBranch = spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+  const gitBranch = spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
 
-//   if (gitBranch.error) {
-//     console.log('Error obteniendo rama activa', child.error);
-//     process.exit(1);
-//   }
+  if (gitBranch.error) {
+    console.log('Error obteniendo rama activa', child.error);
+    process.exit(1);
+  }
   
-//   // Nombre de la rama
-//   const branchName = gitBranch.stdout.toString('utf8').trim();
-//   console.log(chalk.bold(`Subiendo rama ${branchName}`));
+  // Nombre de la rama
+  const branchName = gitBranch.stdout.toString('utf8').trim();
+  console.log(chalk.bold(`Subiendo rama ${branchName}`));
 
-//   const pushGit = spawnSync('git', ['push', 'origin', `${branchName}`]);
-//   if (pushGit.error) {
-//     console.log(chalk.red.bold('Ocurrió un error subiendo a Git'));
-//     process.exit(1);
-//   }
+  const pushGit = spawnSync('git', ['push', 'origin', `${branchName}`]);
+  if (pushGit.error) {
+    console.log(chalk.red.bold('Ocurrió un error subiendo a Git'));
+    process.exit(1);
+  }
 
-//   console.log(chalk.bold(`Rama subida con éxito`));
-//   const pushGitTags = spawnSync('git', ['push', '--tags']);
-//   if (pushGitTags.error) {
-//     console.log(chalk.red.bold('Ocurrió un error subiendo los tags a Git'));
-//     process.exit(1);
-//   }
+  console.log(chalk.bold(`Rama subida con éxito`));
+  const pushGitTags = spawnSync('git', ['push', '--tags']);
+  if (pushGitTags.error) {
+    console.log(chalk.red.bold('Ocurrió un error subiendo los tags a Git'));
+    process.exit(1);
+  }
 
-//   console.log(chalk.bold(`Tag subido con éxito`));
-// }
+  console.log(chalk.bold(`Tag subido con éxito`));
+}
 
-// function gitUpdate() {
-//   const addGit = spawnSync('git', ['add', '-A']);
-//   if (addGit.error) {
-//     console.log(chalk.red.bold('Ocurrió un error creando el tag de Git'));
-//     process.exit(1);
-//   }
-//   const commitGit = spawnSync('git', ['commit', '-m', `Actualizada versión: (${packageJson.version})`]);
-//   if (commitGit.error) {
-//     console.log(chalk.red.bold('Ocurrió un error creando el tag de Git'));
-//     process.exit(1);
-//   }
-//   const tagGit = spawnSync('git', ['tag', `${packageJson.version}`]);
-//   if (tagGit.error) {
-//     console.log(chalk.red.bold('Ocurrió un error creando el tag de Git'));
-//     process.exit(1);
-//   }
-// }
+function gitUpdate() {
+  const addGit = spawnSync('git', ['add', '-A']);
+  if (addGit.error) {
+    console.log(chalk.red.bold('Ocurrió un error creando el tag de Git'));
+    process.exit(1);
+  }
+  const commitGit = spawnSync('git', ['commit', '-m', `Actualizada versión: (${packageJson.version})`]);
+  if (commitGit.error) {
+    console.log(chalk.red.bold('Ocurrió un error creando el tag de Git'));
+    process.exit(1);
+  }
+  const tagGit = spawnSync('git', ['tag', `${packageJson.version}`]);
+  if (tagGit.error) {
+    console.log(chalk.red.bold('Ocurrió un error creando el tag de Git'));
+    process.exit(1);
+  }
+}
 
-// function analizeResponse(response) {
-//   switch (response.version) {
-//     case 'patch':
-//       version[2] = `${(+m) + 1}`;
-//       preversion[1] = undefined;
-//       break;
-//     case 'minor':
-//       version[1] = `${(+n) + 1}`;
-//       version[2] = '0';
-//       preversion[1] = undefined;
-//       break;
-//     case 'major':
-//       version[0] = `${(+p) + 1}`;
-//       version[1] = '0';
-//       version[2] = '0';
-//       preversion[1] = undefined;
-//       break;
-//     case 'preupdate':
+function analizeResponse(response) {
+  switch (response.version) {
+    case 'patch':
+      version[2] = `${(+m) + 1}`;
+      preversion[1] = undefined;
+      break;
+    case 'minor':
+      version[1] = `${(+n) + 1}`;
+      version[2] = '0';
+      preversion[1] = undefined;
+      break;
+    case 'major':
+      version[0] = `${(+p) + 1}`;
+      version[1] = '0';
+      version[2] = '0';
+      preversion[1] = undefined;
+      break;
+    case 'preupdate':
 
-//       if (preversion[1]) {
+      if (preversion[1]) {
 
-//         preversion[1] = (+preversion[1]) + 1;
-//       } else {
+        preversion[1] = (+preversion[1]) + 1;
+      } else {
 
-//         preversion[1] = '0';
-//       }
+        preversion[1] = '0';
+      }
 
-//       break;
-//   }
-// }
+      break;
+  }
+}
