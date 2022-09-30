4da7a8c1e8eabd72252740c90f7203726500b668
diegokraenau <diegokraenau@gmail.com>
2022-09-30 10:00:08 -0500

    cicd v1.0

--- routes.js
+++ routes.js
@@ -1,12 +1,11 @@
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send('Hello from app-test-1!')
-    })
-      
-      app.get('/test', (req, res) => {
-        res.send('Testing routes app-test-1!')
-    })
-      
- 
-    return app; 
- };
+  app.get("/", (req, res) => {
+    res.send("Hello from app-test-1!");
+  });
+
+  app.get("/test", (req, res) => {
+    res.send("Testing routes app-test-1!");
+  });
+
+  return app;
+};
