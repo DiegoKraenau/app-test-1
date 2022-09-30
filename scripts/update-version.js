347cb828c181b33184e1396b1666a69698b8a775
diegokraenau <diegokraenau@gmail.com>
2022-09-30 16:19:59 -0500

    asfsdf

---
+++ scripts/update-version.js
@@ -0,0 +1,36 @@
+const prompts = require('prompts');
+const fs = require('fs');
+const {
+  spawnSync
+} = require('child_process');
+
+const question = [{
+    type: 'select',
+    name: 'version',
+    message: `Seleccione la versión a subir.`,
+    choices: [{
+        title: `Patch:`,
+        value: 'patch'
+      },
+      {
+        title: `Minor:`,
+        value: 'minor'
+      },
+      {
+        title: `Major:`,
+        value: 'major'
+      }
+    ]
+  }];
+  
+  (async () => {
+  
+    const response = await prompts(question);
+  
+    if (!response.version) {
+      console.log(chalk.red.bold('No se seleccionó ninguna opción'));
+      process.exit(1);
+    }
+  
+  
+  })();
